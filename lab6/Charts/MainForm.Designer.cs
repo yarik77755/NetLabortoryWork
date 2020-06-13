b9cb9cf69922a9022e0bb7df35596a36e1277004
yarik77755 <yardhd@gmail.com>
2020-06-13 09:37:54 +0300

    added lab6

---
+++ lab6/Charts/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Charts
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea2.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea2);
+            this.chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart.Location = new System.Drawing.Point(0, 0);
+            this.chart.Name = "chart";
+            this.chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Color = System.Drawing.Color.Black;
+            series2.Name = "Series1";
+            this.chart.Series.Add(series2);
+            this.chart.Size = new System.Drawing.Size(800, 450);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            title2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title2.Name = "Title1";
+            title2.Text = "график функции y=23x^2-33, step=0.01";
+            this.chart.Titles.Add(title2);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "MainForm";
+            this.Text = "y=23x^2-33";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
+
---
+++ lab6/Charts/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Charts
+{
+    public partial class MainForm : Form
+    {
+        private Queue<PointDouble> points = new Queue<PointDouble>();
+        static object locker = new object();
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Thread ThreadCalcultation = new Thread(new ThreadStart(MakeCalculation));
+            ThreadCalcultation.Start();
+
+            Thread ThreadChart = new Thread(new ThreadStart(MakeChart));
+            ThreadChart.Start();
+        }
+
+        private void MakeCalculation()
+        {
+            lock (locker)
+            {
+                for (double x = -10.00; x < 10.00; x += 0.01)
+                    points.Enqueue(new PointDouble { X = x, Y = 23 * x * x - 33 });
+            }
+        }
+
+        private void MakeChart()
+        {
+            lock (locker)
+            {
+                for (double x = -10.00; x < 10.00; x += 0.01)
+                {
+                    PointDouble p = points.Dequeue();
+                    chart.Series[0].Points.AddXY(p.X, p.Y);
+                }
+            }
+        }
+
+        struct PointDouble
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+        }
+    }
+}
